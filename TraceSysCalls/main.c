d7a1b30c7b164d5fb6629d4e5f85d694fef05c0b
ereio <git@ere.io>
2015-10-19 02:03:07 -0400

    Part 1 completed

--- TraceSysCalls/main.c
+++ TraceSysCalls/main.c
@@ -2,18 +2,36 @@
 
 int main(int argc, char argv[]) {
 	
-	int x = 10;
-	int y = -2;
+	int x = 1;
 	char* phrase = "All work and no play makes Jack a dull boy\n";
-	x = y + x + x;
-
-	printf("%d", x);
-
-	y = y + x + y;
-
-	printf("%s | %d", phrase, y); 
-
 	
+	x = x + 1;
+
+	printf("%d, This\n", x);
+
+	x = x + 1;
+
+	printf("%d, Should\n", x);
+
+	x = x + 1;
+
+	printf("%d, Have\n", x);
+
+	x = x + 1;
+
+	printf("%d, 10+\n", x);
+
+	x = x + 1;
+
+	printf("%d, System\n", x);
+
+	x = x + 1;
+
+	printf("%d, Calls\n", x);
+
+	x = x + 1;
+
+	printf("%d, %s\n", x, phrase);
 
 	return 0;
 } 
