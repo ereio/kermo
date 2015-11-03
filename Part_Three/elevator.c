9d08679a04e2dbeb36e67779d0b0138f6745c42d
ereio <git@ere.io>
2015-11-03 07:11:26 -0500

    module printing and elevator structures implemented

--- Part_Three/elevator.c
+++ Part_Three/elevator.c
@@ -5,6 +5,7 @@
 #include <linux/slab.h>
 #include <linux/string.h>
 #include <asm-generic/uaccess.h>
+#include "elevator.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("TEAM BUBDUB");
@@ -13,72 +14,115 @@ MODULE_DESCRIPTION("Elevator scheduling service");
 #define ENTRY_NAME "elevator"
 #define PERMS	0644
 #define PARENT NULL
+#define MAXLEN 2048
 
 static struct file_operations fops;
 
+static struct elevator_type elevator;
+static struct building_type building;
+
 static char * current_msg;
+static char * print_move;
+static int len_msg;
 
 static int read_p;
-static int called = 0;
 
 void get_movement(char* message){
-
+	
+	switch(elevator.movement){
+		case IDLE:
+			strcpy(print_move, "IDLE");
+		case UP:
+			strcpy(print_move, "UP");
+		case DOWN:
+			strcpy(print_move, "DOWN");
+		case LOADING:
+			strcpy(print_move, "LOADING");
+		case STOPPED:
+			strcpy(print_move, "STOPPED");
+		break;
+		
+	}
+	
+	len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, "Movement state: %s\n", print_move); 
 }
 
 void get_floor(char* message){
 
+    len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, "Current Floor: %d\n", elevator.floor);
 }
 
 void get_target(char* message){
 
+    len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, "Next Floor: %d\n", elevator.target); 
 }
 
 void get_load(char* message){
 
+    len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, 
+		"Current Weight: %d\nPassengers: %d", elevator.load, elevator.occupancy); 
 }
 
 void get_total_waiting(char* message){
+    int i = 0;
 
+    len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, "\n--- Building Stats ---\n");
+
+    for(i=0; i < 10; i++){
+    	len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, 
+		"Floor %d: %d\n", i, building.waiting[i]); 
+    }
 }
 
 void get_total_serviced(char* message){
 
+    len_msg += snprintf(current_msg + len_msg, MAXLEN-len_msg, 
+		"Total Serviced: %d\n", building.serviced); 
 }
 
-int elevator_open(struct inode *sp_inode, struct file *sp_file){
+int elevator_open(struct inode *sp_inode, struct file *sp_file){	
+	int i;
 	printk("elevator called open\n");
 
 	read_p = 1;
-	current_msg = kmalloc(sizeof(char) * 1024, __GFP_WAIT | __GFP_IO | __GFP_FS);
+	current_msg = kmalloc(sizeof(char) * MAXLEN, __GFP_WAIT | __GFP_IO | __GFP_FS);
+	print_move = kmalloc(sizeof(char) * 50, __GFP_WAIT | __GFP_IO | __GFP_FS);
 
 	if(current_msg == NULL){
 		printk("ERROR, elevator_open");
 		return -ENOMEM;
 	}
+
+	/* Initalize */	
+	for(i = 0; i < 10; i++)
+		building.waiting[i] = (i * 2) % 4;
 	
+	building.serviced = 0;
+
+	elevator.floor = 1;
+	elevator.target = 10;
+	elevator.load = 0;
+	
+	elevator.movement = IDLE;
 	return 0;
 }
 
 ssize_t elevator_read(struct file *sp_file, char __user *buf, size_t size, loff_t *offset){
-
-	int maxlen = 255;
-	int cur_len = 0;
-	int len = 0;
-	
-	struct timeval time;
-	
+	int len;
 	/* Read loops until you return 0*/
 	read_p = !read_p;
 	if(read_p) return 0;
 	printk("elevator called read\n");
+	
+	len_msg = 0;
+	len_msg += snprintf(current_msg, MAXLEN, "\n--- Elevator ---\n");
+	get_movement(current_msg);
+	get_floor(current_msg);
+	get_target(current_msg);
+	get_load(current_msg);
 
-	get_movement(&current_msg);
-	get_floor(&current_msg);
-	get_target(&current_msg);
-	get_load(&current_msg);
-
-	get_total_waiting(&current_msg);
-	get_total_serviced(&current_msg);
+	get_total_waiting(current_msg);
+	get_total_serviced(current_msg);
 
 	len = strlen(current_msg);
 	copy_to_user(buf, current_msg, len);
