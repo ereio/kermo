365c675a2d0cb3a56b50beddb83e9bbc72ad3300
ereio <git@ere.io>
2015-11-04 00:39:52 -0500

    cleaning

--- test_kernel/elevator/.elevator.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/hubbub/Kernel/kermo/test_kernel/elevator/elevator.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/hubbub/Kernel/kermo/test_kernel/elevator/elevator.ko /home/hubbub/Kernel/kermo/test_kernel/elevator/elevator.o /home/hubbub/Kernel/kermo/test_kernel/elevator/elevator.mod.o
--- test_kernel/elevator/.tmp_versions/elevator.mod
+++
@@ -1,2 +0,0 @@
-/home/hubbub/Kernel/kermo/test_kernel/elevator/elevator.ko
-/home/hubbub/Kernel/kermo/test_kernel/elevator/src/module.o
