16cf42be96b2c34a3798369c32f341c9972f4756
Zach Diemer <zach.diemer@gmail.com>
2015-11-04 19:34:59 -0500

    Restructured folders

--- Part_Three/elevator_testing/elevator_calls.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ELEVATOR_CALLS_H
-#define __ELEVATOR_CALLS_H
-
-int start_elevator();
-int issue_request(int type, int start, int dest);
-int stop_elevator();
-
-#endif /*__ELEVATOR_CALLS_H*/
